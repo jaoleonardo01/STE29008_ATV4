576693c3f87a91b593959a5aad8213d0bc218145
root <root@DESKTOP-KGJEAE6.localdomain>
2022-02-04 21:43:54 -0300

    adquirir temp. a cada min

--- main.cpp
+++ main.cpp
@@ -9,10 +9,24 @@ UART uart; //9600 8N1
 Cycle_Counter ccounter;
 ADC_Channel adc(0);
 
+const float U1 = 5;
+const int ADC_MAX = 1023;
+const float R2 = 10000;
+
+const float SHH_A = 0.0010157;
+const float SHH_B = 0.00025690;
+const float SHH_C = -0.0000000097097;
+
 void delay500()
 {
     unsigned long i = 0x7ffff;
     while(i--);
+}
+
+void delay60000()
+{
+    unsigned long i2 = 0x3bfff88;
+    while(i2--);
 }
 
 int main(void)
@@ -22,20 +36,28 @@ int main(void)
     unsigned int adc_value;
     
 	//3-coletar coeficientes via UART
-	uart.puts("DIGITE OS COEFICIENTES SEM ESPACO\n");
+    uart.puts("DIGITE OS COEFICIENTES SEM ESPACO\n");
 	
     uint8_t bufRecSize = 5;
     char bufRec[bufRecSize];
 
-    // get line from UART
     uart.getLine(bufRec, bufRecSize);
 
-    // echo input
     uart.puts("Valores recebidos: ");
     uart.puts(bufRec);
     uart.puts('\n');
 	
-	
+	//1 - adquirir temp. a cada min.
+    while(true)
+    {
+		adc_value = adc.sample();
+		float U2 = f(adc_value);
+		float R1 = g(U2);
+		float T = h(R1);
+		
+		
+		delay60000();
+		
 	while(true)
     {
         uart.puts("oi\n");
@@ -52,4 +74,18 @@ int main(void)
         uart.puts("tchau\n");
         delay500();
     }
+}
+
+float f(float x){
+	return U1 * x / ADC_MAX;
+}
+
+float g(float x){
+	return U1 * R2 / x - R2;
+}
+
+float h(float x){
+	float logX = log(x);
+	float T = SHH_A + SHH_B*logX + SHH_C*logX*logX*logX;
+	return 1.0/T - 273.15;
 }
